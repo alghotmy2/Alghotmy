7d8a3a3e16e3d4429885af691ac6b299fb9f1c0e
alghotmy2 <53632462+alghotmy2@users.noreply.github.com>
2019-08-02 22:56:43 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,7 @@
+import { bookInfo } from "../../app/bookInfo";
+export const MYBOOKS: bookInfo[] = 
+[{ bookname: "Where the Crowding Sing", author: "Delia Owns",bookGenre: "Drama",year: "2007",picture:"./assets/images/pic1.jpg"},
+   {bookname: "The perfect nany", author: "Leilia Selimani",bookGenre: "Romance",year: "2011",picture:"./assets/images/pic2.jpg"} , 
+   {bookname: "How to study smart", author: "Doglus P",bookGenre: "Technical",year: "2015",picture:"./assets/images/pic3.png"}, 
+  {bookname: "Gone Girl", author: "Maria Patrick",bookGenre: "Action",year: "2016",picture:"./assets/images/pic4.jpg"} , 
+]
